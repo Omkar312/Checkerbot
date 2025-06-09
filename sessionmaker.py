888d4d970c07744ea61cbf8668d10221f4e214f6
Omkar312 <114221677+Omkar312@users.noreply.github.com>
2025-06-09 13:59:14 +0530

    Update sessionmaker.py

--- sessionmaker.py
+++ sessionmaker.py
@@ -2,8 +2,8 @@ from telethon import TelegramClient
 from telethon.sessions import StringSession
 
 
-API_KEY = int(input("INSERT YOUR API KEY-->"))
-API_HASH = input("INSERT YOUR API HASH-->") 
+API_KEY = int(input("26084567"))
+API_HASH = input("3cf13649bbf0e931e40fde340768bb64") 
 
 
 bot = TelegramClient(StringSession(), API_KEY, API_HASH)
